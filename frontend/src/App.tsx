815531e57cad494a20f2cc08496e919031fa7719
Jer Heseoh R. Arsolon <heseoh.arsolon@elinnovtech.com>
2025-07-11 13:55:44 +0800

    feat: add google analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,24 @@
 import { skills, experience, projects } from "./constants/Constants";
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Experience from "./sections/Experience"
 import Projects from "./sections/Projects";
 import ContactAndFooter from "./sections/ContactAndFooter";
+import ReactGA from "react-ga4";
+
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   }
+
+  useEffect(() => {
+    ReactGA.initialize("G-5QTV2CW04N");
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
 
   return (
     <div className={`min-h-screen ${isDarkMode ? 'bg-gray-900' : 'bg-gray-50'}`}>
